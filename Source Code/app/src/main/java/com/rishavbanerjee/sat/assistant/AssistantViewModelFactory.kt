0356e21dc2d08c85160e4a8cb01928791456ec2c
Rishav Banerjee <83203285+rishavhub@users.noreply.github.com>
2022-12-28 13:18:21 +0530

    Add files via upload

---
+++ Source Code/app/src/main/java/com/rishavbanerjee/sat/assistant/AssistantViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.rishavbanerjee.sat.assistant
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.rishavbanerjee.sat.data.AssistantDao
+
+class AssistantViewModelFactory(
+    private val dataSource: AssistantDao,
+    private val application: Application
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AssistantViewModel::class.java)) {
+            return AssistantViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
---
+++ Source Code/app/src/main/java/com/rishavbanerjee/sat/data/Assistant.kt
@@ -0,0 +1,18 @@
+package com.rishavbanerjee.sat.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "assistant_messages_table")
+data class Assistant(
+
+    @PrimaryKey(autoGenerate = true)
+    var assistantId: Long = 0L,
+
+    @ColumnInfo(name = "assistant_message")
+    var assistant_message: String = "DEFAULT_MESSAGE",
+
+    @ColumnInfo(name = "human_message")
+    var human_message: String = "DEFAULT_MESSAGE"
+)
